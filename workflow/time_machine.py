66585d75e88ffa4a7ed5863e0842dc457c9419bc
cumelmell <cumelmell@foxmail.com>
2021-03-11 03:22:28 +0800

    feat: execute toogle throttle
    Signed-off-by: cumelmell <cumelmell@foxmail.com>

--- workflow/time_machine.py
+++ workflow/time_machine.py
@@ -4,17 +4,38 @@
 Author: Cumelmell
 Date: 2021-03-10 23:41:20
 LastEditors: Cumelmell
-LastEditTime: 2021-03-11 00:00:01
+LastEditTime: 2021-03-11 03:18:25
 '''
 import os
 
+from .config import global_config
+
 
 class TimeMachine:
+  _is_throttle = False
+
   def __init__(self):
     self.get_throttle_status()
 
+  @property
+  def is_throttle(self):
+    return self._is_throttle
+
+  @is_throttle.setter
+  def is_throttle(self, throttle):
+    TimeMachine._is_throttle = throttle
+
+
+  # 获取时间机器的节流状态
   @staticmethod
   def get_throttle_status():
-    print(1)
-    print(os.popen('ls').readlines())
-    # return
+    try:
+      command_throttle = global_config.getRaw('command', 'commond_throttle')
+      throttle_res = os.popen(command_throttle).read()
+      TimeMachine._is_throttle = is_throttle = bool(int(throttle_res[-2]))
+      return is_throttle
+    except Exception as e:
+      print(e)
+      return False
+
+time_machine = TimeMachine()
--- workflow/workflows.py
+++ workflow/workflows.py
@@ -4,19 +4,42 @@
 Author: Cumelmell
 Date: 2021-03-10 23:30:12
 LastEditors: Cumelmell
-LastEditTime: 2021-03-11 00:06:30
+LastEditTime: 2021-03-11 03:20:26
 '''
-from time_machine import TimeMachine
+import os
+
+from .time_machine import time_machine
+from .config import global_config
 
 class Workflows:
   """
   时间机器工作流
   """
   def __init__(self):
-    self.check_throttle_status()
+    self.status = self.check_throttle_status()
+    self.execute_toggle()
+    self.output_text()
 
   """
   获取时间机器节流状态
   """
-  def check_throttle_status():
-    TimeMachine.get_throttle_status()
+  def check_throttle_status(self):
+    return time_machine.is_throttle
+
+  def execute_toggle(self):
+    try:
+      open = 1
+      if self.status:
+        open = 0
+      command = global_config.getRaw('command', 'command_switch') + '=' + str(open)
+      # print(command)
+      os.popen(command)
+    except Exception as e:
+      print(e)
+
+  """
+  获取提示文本
+  """
+  def output_text(self):
+    tips = global_config.getRaw('messages', 'close_tips' if self.status else 'open_tips')
+    print(tips)
